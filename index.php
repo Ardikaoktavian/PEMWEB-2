8b2709a24f7cad08b310d642e46fe82defa6daf9
Ardikaoktavian <ardikaardika17436@gmail.com>
2023-02-21 14:54:42 +0700

    latihan & tugas php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP Pertemuan 1</title>
+</head>
+<body>
+    <?php
+    $nama = 'Ardika Oktavian';
+    $umur = 19;
+    $berat = 50;
+
+    echo 'Nama Saya : ' .$nama;
+    echo ' <br/> Umur Saya : ' .$umur;
+    echo ' <br/> Berat  Saya : ' .$berat;
+
+    ?>
+</body>
+</html>
