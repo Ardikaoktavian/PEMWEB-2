7a211355212b222ff315fcd352874d0c382cd990
Ardikaoktavian <ardikaardika17436@gmail.com>
2023-03-04 19:55:45 +0700

    Perbarui grade nilai

--- praktikum2/index2.php
+++ praktikum2/index2.php
@@ -7,6 +7,7 @@
     $_uas = $_POST['uas'] ;
     $_tugas = $_POST['tugas'] ;
     $_total = ($_uts * 30 / 100) + ($_uas * 35 / 100) + ($_tugas * 35 / 100);
+    $_range = (($_uts+$_uas+$_tugas)/3);
     echo '<br>  =================================';
     echo '<br> Nama Mahasiswa : ' .$_nama;
     echo '<br> Mata Kuliah :    ' .$_matkul;
@@ -25,47 +26,50 @@
     } else {
         $grade = "TIDAK LULUS";
     }
-    echo '<br> <br>   Persentase Nilai    :   '   .$_total  , "%"    ;
+    // MAHASISWA  DINYATAKAN LULUS APABILA PERSENTASE MELEBIHI 55%
+    // DENGAN KETENTUAN 30% UTS - 35% UAS- 35% TUGAS
+    echo '<br> <br>   Persentase Nilai    :   '   .$_total  , "%" ;
+    echo '<br>  Range Nilai :   '   . number_format($_range,0,',',',',);
     echo '<br> Kamu  Dinyatakan    :   '  .$grade;
 ?>
 
 
-<!-- MODIFIKASI  >      GRADE NILAI -->
+<!-- MODIFIKASI >    RANGE NILAI & GRADE -->
 <?php
-    //PROSES EKSEKUSI NILAI UTS, UAS, TUGAS
-    if($_uts && $_uas && $_tugas >= 85    &&  $_uts && $_uas && $_tugas <= 100){
-        $grade = "A";
-    }elseif($_uts && $_uas && $_tugas >=70     && $_uts && $_uas && $_tugas <= 84){
-        $grade = "B";
-    }elseif($_uts && $_uas && $_tugas >= 56   &&  $_uts && $_uas && $_tugas <= 69){
-        $grade = "C";
-    }elseif($_uts && $_uas && $_tugas >= 36   &&  $_uts && $_uas && $_tugas <= 55){
-        $grade = "D";
-    }elseif($_uts && $_uas && $_tugas >= 0    &&  $_uts && $_uas && $_tugas <=35){
-        $grade = "E";
-    }elseif($_uts && $_uas && $_tugas < 0 &&  $_uts && $_uas && $_tugas >100){
-        $grade = "I";
+// PROSES EKSEKUSI RANGE NILAI UTS, UAS, TUGAS
+    if($_range >=85 && $_range <=100){
+        $rn = "A";
+    }elseif($_range >=70     && $_range <= 84){
+        $rn = "B";
+    }elseif($_range >= 56   &&  $_range <= 69){
+        $rn = "C";
+    }elseif($_range >= 36   &&  $_range <= 55){
+        $rn = "D";
+    }elseif($_range >= 0    &&  $_range <=35){
+        $rn = "E";
+    }elseif($_range < 0 &&  $_range >100){
+        $rn = "I";
     }else{
-        $grade = "Ada Yang Salah Dengan Penilaian Kamu";
+        $rn = "Ada Yang Salah Dengan Penilaian Kamu";
     }
-    echo '<br> <br> Grade Nilai Kamu Adalah :   '   .$grade;
+    echo '<br> <br> Grade Nilai Kamu Adalah :   '   .$rn;
 ?>
 
 
 <!-- MODIFIKASI  >    PREDIKAT NILAI -->
 <?php
     //PROSES EKSEKUSI PREDIKAT NILAI DARI HASIL GRADE
-    if($grade == "A"){
+    if($rn == "A"){
         $predikat = "Sangat Memuaskan";
-    }elseif($grade == "B"){
+    }elseif($rn == "B"){
         $predikat = "Memuaskan";
-    }elseif($grade == "C"){
+    }elseif($rn == "C"){
         $predikat = "Cukup";
-    }elseif($grade == "D"){
+    }elseif($rn == "D"){
         $predikat = "Kurang";
-    }elseif($grade == "E"){
+    }elseif($rn == "E"){
         $predikat = "Sangat Kurang";
-    }elseif($grade == "I"){
+    }elseif($rn == "I"){
         $predikat = "Tidak Ada Grade";
     }else{
         $predikat = "Tidak Ada";
