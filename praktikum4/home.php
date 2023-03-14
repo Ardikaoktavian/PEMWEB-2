8a3637a8dcc357441af1550e6fc47fc597108867
Ardikaoktavian <ardikaardika17436@gmail.com>
2023-03-14 18:06:36 +0700

    Praktikum 4 | AdminLTE

---
+++ praktikum4/home.php
@@ -0,0 +1,70 @@
+<?php
+    include_once 'template/header.php';
+?>
+
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Name</th>
+      <th scope="col">Nomor HandPhone</th>
+      <th scope="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark Jukeberg</td>
+      <td>0112-3475-6554</td>
+      <td>@mrkjkbrg</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob Funny</td>
+      <td>0118-9665-9977</td>
+      <td>@jcbfny</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry Potter</td>
+      <td>0110-1678-9876</td>
+      <td>@lrryptr</td>
+    </tr>
+  </tbody>
+</table>
+
+<table class="table">
+  <thead class="thead-light">
+    <tr>
+    <th scope="col">No</th>
+      <th scope="col">Name</th>
+      <th scope="col">Nomor HandPhone</th>
+      <th scope="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Hand Lee</td>
+      <td>0114-5647-8779</td>
+      <td>@hndle</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Brian Heady</td>
+      <td>0119-4466-7898</td>
+      <td>@brnhdy</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Jenry Huper</td>
+      <td>0113-6534-8866</td>
+      <td>@jnryhpr</td>
+    </tr>
+  </tbody>
+</table>
+
+
+<?php
+    include_once 'template/footer.php';
+?>
