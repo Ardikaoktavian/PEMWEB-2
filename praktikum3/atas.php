681a2b84625a224af67a300982cac85fd2cc917d
Ardikaoktavian <ardikaardika17436@gmail.com>
2023-03-30 08:16:39 +0700

    Praktikum 3 |Fungsi

--- praktikum3/atas.php
+++ praktikum3/atas.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pertemuan Ke-3</title>
 </head>
+
 <body>
     <header>
         <h2>Student Activity Score - STT NF</h2>
