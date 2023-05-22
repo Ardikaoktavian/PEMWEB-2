e4877176c30068eaadebfa179f3248249ce50a00
Ardikaoktavian <114373734+Ardikaoktavian@users.noreply.github.com>
2023-05-22 08:08:43 +0700

    Delete storage directory

--- storage/framework/views/287edb74bb8dbbdb24a3243eb124990c.php
+++
@@ -1,3 +0,0 @@
-<h2>HALAMAN DATA PRODUK</h2>
-
-ID : <?= $id ;  ?><?php /**PATH D:\xampp\htdocs\weblara\example-app\resources\views/produk/index.blade.php ENDPATH**/ ?>
--- storage/framework/views/8456880ee6aa6478adafd17e44736cca.php
+++
@@ -1,3 +0,0 @@
-<h2>INI HALAMAN PROFILE USER</h2>
-
-Kategori User : <?=  $kategoriuser ?><?php /**PATH D:\xampp\htdocs\weblara\example-app\resources\views/user/index.blade.php ENDPATH**/ ?>
